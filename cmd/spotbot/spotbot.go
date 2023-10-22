e19da37dc620b77e533d02376f8a06c85c0c4d49
zdim <zdim227@gmail.com>
2023-10-22 13:58:55 -0600

    move to using .env file

--- cmd/spotbot/spotbot.go
+++ cmd/spotbot/spotbot.go
@@ -8,12 +8,11 @@ import (
 	"os/signal"
 	"zdim/spotbot/pkg/api"
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 var (
 	GuildID  = flag.String("guild", "", "Test Guild ID")
-	BotToken = flag.String("token", "", "Bot Token")
-	AppID    = flag.String("app", "", "App ID")
 )
 
 func init() {
@@ -23,7 +22,15 @@ func init() {
 func main() {
 	fmt.Println("starting bot...")
 
-	session, err := discordgo.New("Bot " + *BotToken)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	botToken := os.Getenv("TOKEN")
+	appId := os.Getenv("APP_ID")
+
+	session, err := discordgo.New("Bot " + botToken)
 	if err != nil {
 		fmt.Println("error starting bot:", err)
 		return
@@ -58,7 +65,7 @@ func main() {
 		}
 	})
 
-	_, err = session.ApplicationCommandCreate(*AppID, *GuildID, &discordgo.ApplicationCommand{
+	_, err = session.ApplicationCommandCreate(appId, *GuildID, &discordgo.ApplicationCommand{
 		Name:        "album",
 		Description: "Post a compact album preview for a provided link or uri",
 	})
